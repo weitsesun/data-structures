19ce577cefc8b4612bef332c9e6a5026e62ed5a2
Wayne Sun <wtse.sun@gmail.com>
2019-04-10 16:01:27 -0700

    finished functional-shared size

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,17 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.amount = 0;
+  obj.size = queueMethods.size;
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+};
 
-
+queueMethods.enqueue = function(){}
+queueMethods.dequeue = function(){}
+queueMethods.size = function(){
+	return this.amount;
+}
