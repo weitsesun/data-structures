75801f6196791c65dee0e28d4160382a6c2d6e63
Wayne Sun <wtse.sun@gmail.com>
2019-04-10 21:50:08 -0700

    finished es6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,30 @@
 class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
+  constructor(amount, storage) {
+  	this.amount = 0;
+  	this.storage = {};
   }
+
+  enqueue(value) {
+	this.storage[this.amount] = value;
+	this.amount++
+	};
+
+	dequeue() {
+		if (this.amount > 0) {
+			var temp = this.storage[0];
+			for (var i = 0; i < this.amount - 1; i ++){
+				this.storage[i] = this.storage[i+1];
+			}
+			delete this.storage[this.amount - 1];
+			this.amount--;
+			return temp;
+		}
+	};
+
+	size() {
+		return this.amount;
+	};
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,27 @@
 class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
-  }
+  constructor(index, storage) {
+  	this.index = 0;
+  	this.storage= {};
+	}
 
+	size() {
+		return this.index;
+	}
+	pop() {
+		if (this.index > 0) {
+		  	var popitem = this.storage[this.index - 1];
+		  	delete this.storage[this.index - 1];
+		  	this.index --; 
+		  	return popitem;
+		} else {
+	  		return ;
+		}  
+	}
+
+	push(value) {
+		this.storage[this.index] = value;
+		this.index++;
+	}
 }
