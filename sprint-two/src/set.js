49e559e7e09f73a4c4b3a138fac0cd7a7d79985f
weitsesun <wtse.sun@gmail.com>
2019-04-12 14:24:29 -0700

    finished set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,22 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage.item = true;
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage.item === true) {return true;}
+  else {return false;}
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage.item;
 };
 
 /*
